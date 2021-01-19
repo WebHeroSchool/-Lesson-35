000095f2e43a82dc2373b5d76464f321df79511a
Rimma <primma1984@mail.ru>
2021-01-19 18:45:25 +0700

    исправила

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-fetch('https://api.github.com/RimmaMaybo');
+let body = document.body;
+let string = window.location.search;
+let url = 'https://api.github.com/users/RimmaMaybo';
+let getName = (url) => {
+  let g = url.split('=');
+  let name = g[1];
+  if (name == underfined) {
+    name = 'RimmaMaybo'
+  }
+  return name;
+}
+
+ fetch(url)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json.avatar_url);
+    console.log(json.name);
+    console.log(json.bio);
+    console.log(json.html_url);
+
+    let img = new Image();
+    img.src = json.avatar_url;
+    body.append(img);
+
+    let name = document.createElement('p');
+    if (json.name != null) {
+      name.innerHTML = json.name;
+    } else {
+      name.innerHTML = 'RimmaMaybo';
+    }
+    body.append(name);
+    name.addEventListener("click", () => window.location = json.html_url);
+  })
+  .catch(err => console.log(err));
