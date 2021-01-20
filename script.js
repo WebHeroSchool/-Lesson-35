e658b5d0e2544a8e0a436ab5c5432a6a532ae7e4
Rimma <primma1984@mail.ru>
2021-01-20 19:06:50 +0700

    исправила

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let body = document.body;
-let string = window.location.search;
-let url = 'https://api.github.com/users/RimmaMaybo';
-fetch(url)
+let url = window.location.href;
+let getName = (url) => {
+  let g = url.split ('=');
+  let name = g[1];
+  if (name == undefined) {
+    name = 'RimmaMaybo'
+  }
+  return name;
+}
+fetch(`https://api.github.com/users/${getName(url)}`)
   .then(res => res.json())
   .then(json => {
     console.log(json.avatar_url);
     console.log(json.name);
     console.log(json.bio);
     console.log(json.html_url);
-
+    
     let img = new Image();
     img.src = json.avatar_url;
     body.append(img);
@@ -21,5 +28,13 @@ fetch(url)
     }
     body.append(name);
     name.addEventListener("click", () => window.location = json.html_url);
+
+    let bio = document.createElement('p');
+    if (json.bio != null) {
+      bio.innerHTML = json.bio;
+    } else {
+      bio.innerHTML = 'Нет данных';
+    }
+    body.append(bio);
   })
   .catch(err => console.log(err));
